5c982ef18579b0b6d7ec0891b11601f5817a44ec
Spa7ky <aberickson101@gmail.com>
2023-05-03 13:24:16 -0700

    IK functions working

--- panda_moveit_controller/src/moveitClient.py
+++ panda_moveit_controller/src/moveitClient.py
@@ -2,11 +2,11 @@
 
 import rospy
 import geometry_msgs.msg
-from abstract_arm.srv import moveToPose, moveToAngles, grip, getJointAngles
+from panda_moveit_controller.srv import moveToPose, moveToAngles, grip, getJointAngles
 from std_msgs.msg import Bool
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import tf2_ros
-from math import pi, isclose
+from math import pi, isclose, sin, cos, radians
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
@@ -115,74 +115,81 @@ class MoveitArmClient:
         # compares each current joint angle to home joint angle - if all match within tolerance then arm is at home 
         return all([isclose(x, y, rel_tol=tol, abs_tol=tol) for x,y in zip(joints1, joints2)])
 
+def makePose(goalT): 
+    # Accepts a homogeneous transformation matrix and outputs a Pose rosmessage
+    r = R.from_matrix(goalT[:3,:3])
+    quaterion = r.as_quat()
+    pose_goal = geometry_msgs.msg.Pose()
+    pose_goal.orientation.x = quaterion[0]
+    pose_goal.orientation.y = quaterion[1]
+    pose_goal.orientation.z = quaterion[2]
+    pose_goal.orientation.w = quaterion[3]
+    pose_goal.position.x = goalT[0,3]
+    pose_goal.position.y = goalT[1,3]
+    pose_goal.position.z = goalT[2,3]
+    print(f"In makePose goalT:\n {goalT}")
+    print(f"In makePose pose_goal:\n {pose_goal}")
+    print("Requesting.. ")
+    
+    return pose_goal
+
+def RX(goalT,angle):
+    R_T = goalT[:3,:3]
+    Rx = np.array(([1,0,0],
+                   [0,cos(radians(angle)),-sin(radians(angle))],
+                   [0,sin(radians(angle)),cos(radians(angle))]))
+    goalT[:3,:3] = R_T @ Rx
+    return goalT
+
+def RY(goalT,angle):
+    R_T = goalT[:3,:3]
+    Ry = np.array(([cos(radians(angle)),0,sin(radians(angle))],
+                   [0,1,0],
+                   [-sin(radians(angle)),0,cos(radians(angle))]))
+    goalT[:3,:3] = R_T @ Ry
+    return goalT
+
+def RZ(goalT,angle):
+    R_T = goalT[:3,:3]
+    Rz = np.array(([cos(radians(angle)),-sin(radians(angle)),0],
+                   [sin(radians(angle)),cos(radians(angle)),0],
+                   [0,0,1]))
+    goalT[:3,:3] = R_T @ Rz
+    return goalT
+
 
 
 if __name__ == "__main__":
     m = MoveitArmClient(init_node=True)
-
-    # pose_goal = geometry_msgs.msg.Pose()
-    # pose_goal.orientation.w = 1.0
-    # pose_goal.position.x = -0.5
-    # pose_goal.position.y = -0.5
-    # pose_goal.position.z = 0.3
-    # print("Requesting.. ")
-    # m.move_arm_EE(pose_goal)
-
-    # pose_goal.orientation.w = 1.0
-    # pose_goal.position.x = 0.4
-    # pose_goal.position.y = -0.2
-    # pose_goal.position.z = 0.7
-    # print("Requesting.. ")
-    # m.move_arm_EE(pose_goal)
-
-    # pose_goal.orientation.w = 1.0
-    # pose_goal.position.x = -0.4
-    # pose_goal.position.y = -0.2
-    # pose_goal.position.z = 0.7
-    # print("Requesting.. ")
-    # m.move_arm_EE(pose_goal)
-    
+ 
     # Panda finger
     finger = 0.054
     m.move_gripper(finger)
 
     # m.is_arm_in_home_pos()
-    init = [1.383035467921843, -1.5321861096780172, -1.7988014563683827, -1.7618231679666039, -1.5773894447485612, 1.8261655306549525, -0.7708965437742608]
+    # init = [1.383035467921843, -1.5321861096780172, -1.7988014563683827, -1.7618231679666039, -1.5773894447485612, 1.8261655306549525, -0.7708965437742608]
     raised = [1.3500737407279217, -1.4550843634153168, -1.69247116861691, -1.8027891555282816, -1.486084602669667, 1.7387179465620655, -0.7726231094421925]
-    final = [2.7512211763054086, -1.3382896714015704, -1.6315563705904341, -1.6254787932375034, -1.322917349269738, 1.5876109064986714, 0.36519625187956084]
-    m.move_arm_angles(init)
+    # final = [2.7512211763054086, -1.3382896714015704, -1.6315563705904341, -1.6254787932375034, -1.322917349269738, 1.5876109064986714, 0.36519625187956084]
+    
+    # move fingers around cube
+    #m.move_arm_angles(init)
+    goalT = np.array(([1, 0, 0, 0.5],
+                      [0, -1, 0, -0.4],
+                      [0, 0, -1, 0.4],
+                      [0, 0, 0, 1]))
+    goalT = RZ(goalT,45)
+    pose_goal = makePose(goalT)
+    m.move_arm_EE(pose_goal)
+
+    # pick up 
     m.move_gripper(0.01)
-    m.move_arm_angles(raised)
-    m.move_arm_angles(final)
-    m.move_gripper(finger)
+    #m.move_arm_angles(raised)
+    
+
+    # Travel Through door
+    #m.move_arm_angles(final)
+
     # goalT = np.array(([1, 0, 0, -2.3],
-    #                 [0, 1, 0, 0],
-    #                 [0, 0, -1, 2.],
-    #                 [0, 0, 0, 1]))
-    
-    # r = R.from_matrix(goalT[:3,:3])
-    # quaterion = r.as_quat()
-
-    # print(quaterion)
-
-    # pose_goal = geometry_msgs.msg.Pose()
-    # pose_goal.orientation.x = quaterion[0]
-    # pose_goal.orientation.y = quaterion[1]
-    # pose_goal.orientation.z = quaterion[2]
-    # pose_goal.orientation.w = quaterion[3]
-    # pose_goal.position.x = 0.7
-    # pose_goal.position.y = 0
-    # pose_goal.position.z = 0.3
-    # print("Requesting.. ")
-    # m.move_arm_EE(pose_goal)
-
-    
-
-    # impossible arm position test
-    # pose_goal = geometry_msgs.msg.Pose()
-    # pose_goal.orientation.w = 1.0
-    # pose_goal.position.x = 0.3
-    # pose_goal.position.y = 0.5
-    # pose_goal.position.z = -0.3
-    # print("Requesting.. ")
-    # m.move_arm(pose_goal)
+  
+    # Release
+    m.move_gripper(finger)
